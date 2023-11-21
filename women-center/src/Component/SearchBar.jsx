1b379acc495ebb2be44b1fd2653b6cec7d54d763
nova.hrt <novahertanti@gmail.com>
2023-11-21 19:08:58 +0700

    beneran kurang integrasi dan routing

--- women-center/src/Component/SearchBar.jsx
+++ women-center/src/Component/SearchBar.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
-
+// SearchBar.jsx
+import React, { useState } from 'react';
+import PopUpModal from './PopUpModal'; // Impor komponen PopUpModal dari file terpisah
 
 const SearchBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="search-bar">
       {/* Ikon pencarian */}
       <div className="search-icon">
-        <img src="public/asset/Search.jpg" alt="Search-Icon" />
+        <img src='public/asset/Search.jpg' alt="Search-Icon" />
       </div>
 
       {/* Kotak pencarian */}
       <input type="text" placeholder="Search..." />
 
+      {/* Ikon filter date */}
+      <div className="filter-date-icon" onClick={togglePopup}>
+        <img src='public/asset/filterdate.png' alt="Filter-Date-Icon" />
+      </div>
+
+      {/* Memanggil komponen PopUpModal */}
+      <PopUpModal isOpen={isOpen} togglePopup={togglePopup} />
     </div>
   );
 };
