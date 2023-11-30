6311bea668757add6f9dce426265a49a6a7e2bc0
hoshiroo <108223413+hoshiroo1804@users.noreply.github.com>
2023-12-01 02:43:31 +0700

    done tinggal hubungin ke popup dan ke page table

--- women-center/src/component/Karir.jsx
+++ women-center/src/component/Karir.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './Karir.css';
 import { FiUploadCloud, FiTrash2 } from "react-icons/fi";
+import { MdEditSquare } from "react-icons/md";
 
 // Main component
 const Karir = () => {
@@ -45,7 +46,7 @@ const Karir = () => {
 
   return (
     <>
-      <button onClick={openModal}>Buka Modal</button>
+      <MdEditSquare color='#F4518D'className="icon-button" onClick={openModal} />
       {isModalOpen && (
         <div className="modal show">
           <div className="modal-content">
@@ -54,11 +55,11 @@ const Karir = () => {
             </span>
 
             <div className='mainText'>
-              <h3 style={{color : 'black'}}>Karier</h3>
+              <h5 style={{ color: 'black' }}>Edit Rekomendasi Karier</h5>
               <form className=' m-5'>
                 <div className='row auto'>
-                  <div className='col-3 text-start' style={{color:'black'}}>
-                  <label htmlFor="profileImage">Foto Profil</label>
+                  <div className='col-3 text-start' style={{ color: 'black' }}>
+                    <label htmlFor="profileImage">Foto Profil</label>
                     <div className='fotoprofile'>
                       <div>
                         {profileImage && (
@@ -83,11 +84,10 @@ const Karir = () => {
                             style={{ display: 'none' }}
                           />
                         </label>
-
                       </div>
                     </div>
                   </div>
-                  <div className='col-6 text-start' style={{marginLeft : '-85px', color:'black'}}>
+                  <div className='col-6 text-start' style={{ marginLeft: '-85px', color: 'black' }}>
                     <label htmlFor="coverImage">Foto Sampul</label>
                     <div className='fotosampul'>
                       <div >
@@ -118,81 +118,99 @@ const Karir = () => {
 
 
                 <div className=' row p-2'>
-                    <div className=' col-4 text-start'>
-                        <label htmlFor="">Nama Karir</label>
-                        <input type="text" className='forminput' /> 
-                        <label htmlFor="">Nama Karir</label>
-                        <input type="text" className='forminput' /> 
-                        <label htmlFor="">Nama Karir</label>
-                        <input type="text" className='forminput' /> 
-                        <label htmlFor="">Nama Karir</label>
-                        <input type="text" className='forminput' />  
+                  <div className=' col-4 text-start'>
+                  <div className="form-group">
+                      <label htmlFor="namaKarir">Nama Karir</label>
+                      <input type="text" className="forminput" id="namaKarir" placeholder="Nama Karir" />
                     </div>
-                    <div className=' col-4 text-start'>
-                    <label htmlFor="">Nama Karir</label>
-                        <input type="text" className='forminput' /> 
-                        <label htmlFor="">Nama Karir</label>
-                        <input type="text" className='forminput' /> 
-                        <label htmlFor="">Nama Karir</label>
-                        <input type="text" className='forminput' /> 
+                    <div className="form-group">
+                      <label htmlFor="tanggalDitambahkan">Tanggal Ditambahkan</label>
+                      <input type="text" className="forminput" id="tanggalDitambahkan" placeholder="Tanggal Ditambahkan" />
                     </div>
-                    <div className=' col-4 text-start '>
-                    <label htmlFor="">Nama Karir</label><br/>
-                        <div className='row '>
-                        <label className='formcheckbox'>
-                        <input type="checkbox" />
-                        Full-Time
-                        </label>
-                        <label>
-                        <input type="checkbox" />
-                        Freelance
-                        </label>
-                        <label>
-                        <input type="checkbox" />
-                        Kontrak
-                        </label>
-                        <label>
-                        <input type="checkbox" />
-                        Magang
-                        </label>
-                        <label>
-                        <input type="checkbox" />
-                        Remote
-                        </label>
-                        <label>
-                        <input type="checkbox" />
-                        Proyek
-                        </label>
-                        <label>
-                        <input type="checkbox" />
-                        Shift
-                        </label>
-                        <label>
-                        <input type="checkbox" />
-                        Sementara
-                        </label>
-                        </div>
+                    <div className="form-group">
+                      <label htmlFor="skillRequirement">Skill Requirement</label>
+                      <input type="text" className="forminput" id="skillRequirement" placeholder="Skill Requirement" />
                     </div>
-                    <div className='row'>
-                        <div className='col-4 text-start '>
-                            <label htmlFor="">About The Job</label> <br/>
-                            <textarea className='formabout' ></textarea>
-                        </div>
-                        <div className='col-4 text-start'>
-                        <label htmlFor="">About The Job</label> <br/>
-                        <textarea className='formabout' ></textarea>
-                        </div>
+                    <div className="form-group">
+                      <label htmlFor="linkLinkedin">Link Linkedin</label>
+                      <input type="text" className="forminput" id="linkLinkedin" placeholder="Link Linkedin" />
                     </div>
-                    <div className='row'>
-                        <div className='col text-end'>
-                            <button className='btnbatal'>Batal</button>
-                            <button className='buttonsave'>Simpan</button>
-                        </div>
+                  </div>
+                  <div className=' col-4 text-start'>
+                    <div className="form-group">
+                      <label htmlFor="namaPerusahaan">Nama Perusahaan</label>
+                      <input type="text" className='forminput' id="namaPerusahaan"  placeholder="Nama Perusahaan" />
                     </div>
+                    <div className="form-group">
+                      <label htmlFor="ukuranPerusahaan">Ukuran Perusahaan Dan Departemen</label>
+                      <input type="text" className='forminput' id="ukuranPerusahaan"  placeholder="Ukuran Perusahaan"/>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="lokasi">Lokasi</label>
+                      <input type="text" className='forminput' id="lokasi"  placeholder="Lokasi" />
+                    </div>
+                  </div>
+                  <div className=' col-4 text-start '>
+                    <label htmlFor="jobType">Job Type</label><br />
+                    <div className='row '>
+                      <div className="form-group formcheckbox">
+                        <input type="checkbox" id="fullTime" />
+                        <label htmlFor="fullTime">Full-Time</label>
+                      </div>
+                      <div className="form-group formcheckbox">
+                        <input type="checkbox" id="freelance" />
+                        <label htmlFor="freelance">Freelance</label>
+                      </div>
+                      <div className="form-group formcheckbox">
+                        <input type="checkbox" id="kontrak" />
+                        <label htmlFor="kontrak">Kontrak</label>
+                      </div>
+                      <div className="form-group formcheckbox">
+                        <input type="checkbox" id="magang" />
+                        <label htmlFor="magang">Magang</label>
+                      </div>
+                      <div className="form-group formcheckbox">
+                        <input type="checkbox" id="remote" />
+                        <label htmlFor="remote">Remote</label>
+                      </div>
+                      <div className="form-group formcheckbox">
+                        <input type="checkbox" id="proyek" />
+                        <label htmlFor="proyek">Proyek</label>
+                      </div>
+                      <div className="form-group formcheckbox">
+                        <input type="checkbox" id="shift" />
+                        <label htmlFor="shift">Shift</label>
+                      </div>
+                      <div className="form-group formcheckbox">
+                        <input type="checkbox" id="sementara" />
+                        <label htmlFor="sementara">Sementara</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='col-4 text-start '>
+                      <div className="form-group">
+                        <label htmlFor="aboutJob">About The Job</label> <br />
+                        <textarea className='formabout' id="aboutJob"  placeholder="About The Job"></textarea>
+                      </div>
+                    </div>
+                    <div className='col-4 text-start'>
+                      <div className="form-group">
+                        <label htmlFor="aboutCompany">About The Company</label> <br />
+                        <textarea className='formabout' id="aboutCompany"  placeholder="About The Company"></textarea>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='row'>
+                    <div className='col text-end'>
+                      <button className='btnbatal'>Batal</button>
+                      <button className='buttonsave'>Simpan</button>
+                    </div>
+                  </div>
                 </div>
-            </form>
-           </div>
-        </div>
+              </form>
+            </div>
+          </div>
         </div>
       )}
     </>
