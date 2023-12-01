d4164917b36fd26a221461ca8823c6316ef6f977
hoshiroo <108223413+hoshiroo1804@users.noreply.github.com>
2023-12-01 14:29:20 +0700

    p

---
+++ women-center/src/component/TableKarir.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const TableKarir = () => {
+return (
+    <>
+    
+    
+
+    
+    </>
+);};
+
+
+
+
+
+export default TableKarir;
