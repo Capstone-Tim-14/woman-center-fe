e63449039f13abf8bfa9c082c04ecdeaaaa6618d
nova.hrt <novahertanti@gmail.com>
2023-12-04 15:42:11 +0700

    header

--- women-center/src/component/organisms/header/headerPaket.jsx
+++ women-center/src/component/organisms/header/headerPaket.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { GoHomeFill } from 'react-icons/go';
+import { IoNotifications } from 'react-icons/io5';
 
-function HeaderPaket() {
-  
+const headerPaket = () => {
   return (
-    <div>headerPaket</div>
-  )
-}
+    <div className="header">
+      <div className="header-content">
+        <h1>Paket</h1>
+        <div className="subtitle">
+          <div className="icon-home">
+            <GoHomeFill className="icon-home-img" />
+          </div>
+          <div className="subtitle-text">/ Paket</div>
+        </div>
+      </div>
+      <div className="header-icons">
+        {/* Notification Icon */}
+        <div className="div-icon">
+          <div className="div-icon-inner">
+            <IoNotifications className="div-icon-bar" />
+          </div>
+        </div>
 
-export default HeaderPaket
+        {/* Profile Image */}
+        <img className="div-image" src="public/asset/profile 1.jpg" alt="Profile Image" />
+      </div>
+    </div>
+  );
+};
+
+export default headerPaket;
