fd1346bd81661beffa59a87f5029334dbc84ac47
Redolthfi <redolthfi@gmail.com>
2023-12-13 16:04:53 +0700

    revisi pagination jadwal

--- women-center/src/components/Organism/SesiLeftContainer.jsx
+++ women-center/src/components/Organism/SesiLeftContainer.jsx
@@ -1,101 +1,154 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "../../styles/sesi-left-konseling.css"
 import imagedashboard from "../../assets/icon/shadow.png"
 import EditKonseling from '../Molekul/Modal/modalEditKonseling'
+import PageOptions from '../Molekul/PageOption';
 
 
-const SesiLeftContainer = () => (
-  <div className="Sesi-konseling">
+const SesiLeftContainer = () => {
+    const [pagination, setPagination] = useState({
+        page: 1,
+        totalPage: 1,
+        nextPage: null,
+        prevPage: null,
+    });
+
+    const goToPage = (page) => {
+        fetchData(page);
+      };
+      const fetchData = async (page = 1, filter = '') => {
+        try {
+          if (token) {
+            const response = await axios.get(
+            //   'https://api-ferminacare.tech/api/v1/admin/articles',
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+                params: {
+                  page,
+                  filter,
+                },
+              }
+            );
+      
+            const data = response.data;
+            
+            // setTableData(data.data);
+      
+            setPagination({
+              page: data.meta.page,
+              totalPage: data.meta.total_page,
+              nextPage: data.meta.next_page,
+              prevPage: data.meta.previous_page,
+            });
+      
+          } else {
+            console.error('Token not available.');
+        
+            logout();
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
+    return (
+        <div className="Sesi-konseling">
             <div className="all-sesi-konseling">
-            <div className="head-dashboard-sesi-konseling">
-                <h6 className="txt-dashboard-sesi-konseling">Sesi Konseling</h6>
-            </div>
-            <div className="head-dashboard-sesi-konseling">
-                <h10 className="txt-dashboard2-sesi-konseling">13 Sesi Hari Ini</h10>
-            </div>
-            <hr  className="garis"/>
-            <div className="showall-sesi-konseling">
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
-                    </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Konselor S.Psi 
-                        <EditKonseling /><br /></h5>
-                        <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
-                        <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
-                    </div>
+                <div className="head-dashboard-sesi-konseling">
+                    <h6 className="txt-dashboard-sesi-konseling">Sesi Konseling</h6>
                 </div>
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
-                    </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Konselor S.Psi 
-                            <EditKonseling /><br /></h5>
-                        <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
-                        <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
-                    </div>
+                <div className="head-dashboard-sesi-konseling">
+                    <h10 className="txt-dashboard2-sesi-konseling">13 Sesi Hari Ini</h10>
                 </div>
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
+                <hr className="garis" />
+                <div className="showall-sesi-konseling">
+                    <div className="show-sesi-konseling" >
+                        <div className="img-sesi-konseling">
+                            <img src={imagedashboard} alt="img-sesi-konseling" />
+                        </div>
+                        <div className="content-sesi-konseling">
+                            <h5 className="title-sesi-konseling">
+                                Nama Konselor S.Psi
+                                <EditKonseling /><br /></h5>
+                            <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
+                            <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
+                            <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                        </div>
                     </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Konselor S.Psi 
-                            <EditKonseling /><br /></h5>
-                        <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
-                        <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                    <div className="show-sesi-konseling" >
+                        <div className="img-sesi-konseling">
+                            <img src={imagedashboard} alt="img-sesi-konseling" />
+                        </div>
+                        <div className="content-sesi-konseling">
+                            <h5 className="title-sesi-konseling">
+                                Nama Konselor S.Psi
+                                <EditKonseling /><br /></h5>
+                            <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
+                            <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
+                            <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                        </div>
                     </div>
-                </div>
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
+                    <div className="show-sesi-konseling" >
+                        <div className="img-sesi-konseling">
+                            <img src={imagedashboard} alt="img-sesi-konseling" />
+                        </div>
+                        <div className="content-sesi-konseling">
+                            <h5 className="title-sesi-konseling">
+                                Nama Konselor S.Psi
+                                <EditKonseling /><br /></h5>
+                            <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
+                            <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
+                            <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                        </div>
                     </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Konselor S.Psi 
-                            <EditKonseling /><br /></h5>
-                        <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
-                        <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                    <div className="show-sesi-konseling" >
+                        <div className="img-sesi-konseling">
+                            <img src={imagedashboard} alt="img-sesi-konseling" />
+                        </div>
+                        <div className="content-sesi-konseling">
+                            <h5 className="title-sesi-konseling">
+                                Nama Konselor S.Psi
+                                <EditKonseling /><br /></h5>
+                            <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
+                            <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
+                            <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                        </div>
                     </div>
-                </div>
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
+                    <div className="show-sesi-konseling" >
+                        <div className="img-sesi-konseling">
+                            <img src={imagedashboard} alt="img-sesi-konseling" />
+                        </div>
+                        <div className="content-sesi-konseling">
+                            <h5 className="title-sesi-konseling">
+                                Nama Konselor S.Psi
+                                <EditKonseling /><br /></h5>
+                            <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
+                            <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
+                            <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                        </div>
                     </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Konselor S.Psi 
-                            <EditKonseling /><br /></h5>
-                        <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
-                        <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                    <div className="show-sesi-konseling" >
+                        <div className="img-sesi-konseling">
+                            <img src={imagedashboard} alt="img-sesi-konseling" />
+                        </div>
+                        <div className="content-sesi-konseling">
+                            <h5 className="title-sesi-konseling">
+                                Nama Konselor S.Psi
+                                <EditKonseling /><br /></h5>
+                            <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
+                            <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
+                            <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                        </div>
                     </div>
-                </div>
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
-                    </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Konselor S.Psi 
-                            <EditKonseling /><br /></h5>
-                        <h5 className="sub-title-sesi-konseling">Nama Paket . Nama User<br /></h5>
-                        <p className="subtitle-sesi-konseling">Sesi Tersisa   : 1 Sesi</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
-                    </div>
+                    <PageOptions onPageChange={goToPage} totalPages={pagination.totalPage || 1} />
+
                 </div>
             </div>
         </div>
-  </div>
-);
+
+    )
+};
 
 export default SesiLeftContainer;
--- women-center/src/components/Organism/SesiRightContainer.jsx
+++ women-center/src/components/Organism/SesiRightContainer.jsx
@@ -1,112 +1,163 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "../../styles/sesi-left-konseling.css"
 import imagedashboard from "../../assets/icon/shadow.png"
+import PageOptions from '../Molekul/PageOption';
 
 
-const SesiRightContainer = () => (
-  <div className="Sesi-acara">
-            <div className="all-sesi-konseling">
-            <div className="head-dashboard-sesi-konseling">
-                <h6 className="txt-dashboard-sesi-konseling">Acara</h6>
-            </div>
-            <div className="head-dashboard-sesi-konseling">
-                <h10 className="txt-dashboard2-sesi-konseling">4 Acara Minggu Ini</h10>
-            </div>
-            <hr  className="garis"/>
-            <div className="showall-sesi-konseling">
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
-                    </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Acara
-                        <button className='button-edit-acara'>
-                            ...
-                        </button><br /></h5>
-                        <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
-                        <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
-                    </div>
-                </div>
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
-                    </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Acara
-                        <button className='button-edit-acara'>
-                            ...
-                        </button><br /></h5>
-                        <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
-                        <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
-                    </div>
-                </div>
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
-                    </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Acara
-                        <button className='button-edit-acara'>
-                            ...
-                        </button><br /></h5>
-                        <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
-                        <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
-                    </div>
-                </div>
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
-                    </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Acara
-                        <button className='button-edit-acara'>
-                            ...
-                        </button><br /></h5>
-                        <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
-                        <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
-                    </div>
-                </div>
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
-                    </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Acara
-                        <button className='button-edit-acara'>
-                            ...
-                        </button><br /></h5>
-                        <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
-                        <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
-                    </div>
-                </div>
-                <div className="show-sesi-konseling" >
-                    <div className="img-sesi-konseling">
-                        <img src={imagedashboard} alt="img-sesi-konseling" />
-                    </div>
-                    <div className="content-sesi-konseling">
-                        <h5 className="title-sesi-konseling">
-                            Nama Acara
-                        <button className='button-edit-acara'>
-                            ...
-                        </button><br /></h5>
-                        <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
-                        <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
-                        <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-  </div>
-);
+const SesiRightContainer = () => {
 
-export default SesiRightContainer;
+        const [pagination, setPagination] = useState({
+            page: 1,
+            totalPage: 1,
+            nextPage: null,
+            prevPage: null,
+        });
+
+        const goToPage = (page) => {
+            fetchData(page);
+        };
+        const fetchData = async (page = 1, filter = '') => {
+            try {
+                if (token) {
+                    const response = await axios.get(
+                        //   'https://api-ferminacare.tech/api/v1/admin/articles',
+                        {
+                            headers: {
+                                Authorization: `Bearer ${token}`,
+                            },
+                            params: {
+                                page,
+                                filter,
+                            },
+                        }
+                    );
+
+                    const data = response.data;
+
+                    // setTableData(data.data);
+
+                    setPagination({
+                        page: data.meta.page,
+                        totalPage: data.meta.total_page,
+                        nextPage: data.meta.next_page,
+                        prevPage: data.meta.previous_page,
+                    });
+
+                } else {
+                    console.error('Token not available.');
+
+                    logout();
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        return (
+            <div className="Sesi-acara">
+                <div className="all-sesi-konseling">
+                    <div className="head-dashboard-sesi-konseling">
+                        <h6 className="txt-dashboard-sesi-konseling">Acara</h6>
+                    </div>
+                    <div className="head-dashboard-sesi-konseling">
+                        <h10 className="txt-dashboard2-sesi-konseling">4 Acara Minggu Ini</h10>
+                    </div>
+                    <hr className="garis" />
+                    <div className="showall-sesi-konseling">
+                        <div className="show-sesi-konseling" >
+                            <div className="img-sesi-konseling">
+                                <img src={imagedashboard} alt="img-sesi-konseling" />
+                            </div>
+                            <div className="content-sesi-konseling">
+                                <h5 className="title-sesi-konseling">
+                                    Nama Acara
+                                    <button className='button-edit-acara'>
+                                        ...
+                                    </button><br /></h5>
+                                <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
+                                <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
+                                <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                            </div>
+                        </div>
+                        <div className="show-sesi-konseling" >
+                            <div className="img-sesi-konseling">
+                                <img src={imagedashboard} alt="img-sesi-konseling" />
+                            </div>
+                            <div className="content-sesi-konseling">
+                                <h5 className="title-sesi-konseling">
+                                    Nama Acara
+                                    <button className='button-edit-acara'>
+                                        ...
+                                    </button><br /></h5>
+                                <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
+                                <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
+                                <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                            </div>
+                        </div>
+                        <div className="show-sesi-konseling" >
+                            <div className="img-sesi-konseling">
+                                <img src={imagedashboard} alt="img-sesi-konseling" />
+                            </div>
+                            <div className="content-sesi-konseling">
+                                <h5 className="title-sesi-konseling">
+                                    Nama Acara
+                                    <button className='button-edit-acara'>
+                                        ...
+                                    </button><br /></h5>
+                                <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
+                                <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
+                                <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                            </div>
+                        </div>
+                        <div className="show-sesi-konseling" >
+                            <div className="img-sesi-konseling">
+                                <img src={imagedashboard} alt="img-sesi-konseling" />
+                            </div>
+                            <div className="content-sesi-konseling">
+                                <h5 className="title-sesi-konseling">
+                                    Nama Acara
+                                    <button className='button-edit-acara'>
+                                        ...
+                                    </button><br /></h5>
+                                <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
+                                <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
+                                <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                            </div>
+                        </div>
+                        <div className="show-sesi-konseling" >
+                            <div className="img-sesi-konseling">
+                                <img src={imagedashboard} alt="img-sesi-konseling" />
+                            </div>
+                            <div className="content-sesi-konseling">
+                                <h5 className="title-sesi-konseling">
+                                    Nama Acara
+                                    <button className='button-edit-acara'>
+                                        ...
+                                    </button><br /></h5>
+                                <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
+                                <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
+                                <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                            </div>
+                        </div>
+                        <div className="show-sesi-konseling" >
+                            <div className="img-sesi-konseling">
+                                <img src={imagedashboard} alt="img-sesi-konseling" />
+                            </div>
+                            <div className="content-sesi-konseling">
+                                <h5 className="title-sesi-konseling">
+                                    Nama Acara
+                                    <button className='button-edit-acara'>
+                                        ...
+                                    </button><br /></h5>
+                                <h5 className="sub-title-sesi-konseling2">Taman Ismail Marzuki, Jakarta<br /></h5>
+                                <p className="subtitle-sesi-konseling">Tanggal         : 9 November 2023</p>
+                                <p className="subtitle-sesi-konseling2">Countdown      : 1 hari, 5 jam 50 menit</p>
+                            </div>
+                        </div>
+                        <PageOptions onPageChange={goToPage} totalPages={pagination.totalPage || 1} />
+                    </div>
+                </div>
+            </div>
+        )
+    };
+
+    export default SesiRightContainer;
