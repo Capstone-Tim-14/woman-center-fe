e2163ec262eac3379be535b8ae9475cd92823312
nadiahindrianti <104046022+nadiahindrianti@users.noreply.github.com>
2023-12-18 08:59:43 +0700

    Update eror

--- women-center/src/components/Molekul/Modal/ModalGagalDataKonselor.jsx
+++ women-center/src/components/Molekul/Modal/ModalGagalDataKonselor.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Modal from 'react-bootstrap/Modal'
 import ButtonsDataKonselor from '../../Atom/button/ButtonsDataKonselor'
-import '../../../styles/modalAlertDataKonselor.css'
+import '../../../styles/ModalAlertDataKonselor.css'
 import image from '../../../assets/icon/Group2.png'
 
 function ModalGagalDataKonselor({Gagal, Close, label}) {
